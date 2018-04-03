5c6bf8b06f5127e826c272af886026e17f1dce34
tawayee <tawayee@gmail.com>
2018-04-02 23:09:24 -0400

    meeting list on homepage

--- xgppdocs/views.py
+++ xgppdocs/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 import os.path
 
+SA2Meetings = [
+    {'name': 'SA2-126', 'time': 'Feb 2018', 'city': 'Montreal',},
+    {'name': 'SA2-125', 'time': 'Jan 2018', 'city': 'Gothenburg',},
+]
 
 def homepage(request):
-    return render(request, 'homepage.html', {})
+    context = {}
+    context['sa2meetings'] = SA2Meetings
+    return render(request, 'homepage.html', context)
