9c2653c2ee2eb77a966a5ebba1d0a3834d90a6cd
tawayee <tawayee@gmail.com>
2018-04-05 22:34:45 -0400

    ..

--- tools/unziptdocs.sh
+++ tools/unziptdocs.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-for zipfile in *.zip; do
-    unzip $zipfile -d ./
-done 
+# for zipfile in *.zip; do
+#    unzip $zipfile -d ./
+# done
+
+for file in S2*.{doc, DOC, docx, DOCX, pdf, PDF}; do
+    echo $file
+done
