a1c611a7c3588cc96d65c0c00b32ca2b909abc1e
tawayee <tawayee@gmail.com>
2018-04-05 23:17:11 -0400

    ..

--- tools/unziptdocs.sh
+++ tools/unziptdocs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# for zipfile in *.zip; do
-#    unzip $zipfile -d ./
-# done
+for zipfile in *.zip; do
+    unzip $zipfile -d ./
+done
 
-for file in S2*.{doc, DOC, docx, DOCX, pdf, PDF}; do
+for file in S*.{doc,DOC,docx,DOCX,pdf,PDF}; do
     filename="$file"
     tdocnum=${filename:0:9}
-    ext=${filename##.*}
+    ext=${filename##*.}
     newfile="$tdocnum"."$ext"
     mv "$file" "$newfile"
     echo $file moved to $newfile
